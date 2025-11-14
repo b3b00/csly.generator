c70a3677b7b029eebb9d23fa06465d224bac08d8
b3b00 <olivier.duhart@gmail.com>
2025-11-14 14:00:06 +0100

    .

--- src/samples/sourceGenerationTester/Program.cs
+++ src/samples/sourceGenerationTester/Program.cs
@@ -1,14 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-using csly.generator.sourceGenerator;
+//using csly.generator.sourceGenerator;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using SharpFileSystem.FileSystems;
 using sourceGenerationTester.expressionParser;
-using sourceGenerationTester.staticlexer;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -24,53 +22,53 @@ public partial class Program
 {
     public static void Main(string[] args)
     {
-        Generate();
+        //Generate();
         Run();
         /*GoStatic();
         Run();*/
     }
 
 
-    private static void Generate()
-    {
+    //private static void Generate()
+    //{
         
-        EmbeddedResourceFileSystem fs = new EmbeddedResourceFileSystem(typeof(Program).Assembly);
-        var parser = fs.ReadAllText("/samples/expression.gram");
+    //    EmbeddedResourceFileSystem fs = new EmbeddedResourceFileSystem(typeof(Program).Assembly);
+    //    var parser = fs.ReadAllText("/samples/expression.gram");
         
-        var result = GenerateSource(parser, "SimpleParser");
+    //    var result = GenerateSource(parser, "SimpleParser");
         
-        var contents = result.GeneratedTrees.ToList().ToDictionary(x => x.FilePath,x => x.ToString());
-        var generatedFiles = result.GeneratedTrees.Select(x => new FileInfo(x.FilePath).Name);
+    //    var contents = result.GeneratedTrees.ToList().ToDictionary(x => x.FilePath,x => x.ToString());
+    //    var generatedFiles = result.GeneratedTrees.Select(x => new FileInfo(x.FilePath).Name);
 
-        foreach (var file in contents)
-        {
-            File.WriteAllText(Path.Combine("c:/tmp/generation/","SimpleParser.cs"), file.Value);
-        }
+    //    foreach (var file in contents)
+    //    {
+    //        File.WriteAllText(Path.Combine("c:/tmp/generation/","SimpleParser.cs"), file.Value);
+    //    }
         
-    }
+    //}
     
-    private static GeneratorDriverRunResult GenerateSource(string source, string className)
-    {
-        // Create an instance of the source generator.
-        var generator = new CslyParserGenerator();
+    //private static GeneratorDriverRunResult GenerateSource(string source, string className)
+    //{
+    //    // Create an instance of the source generator.
+    //    var generator = new CslyParserGenerator();
 
-        // Source generators should be tested using 'GeneratorDriver'.
-        var driver = CSharpGeneratorDriver.Create(new[] { generator });
+    //    // Source generators should be tested using 'GeneratorDriver'.
+    //    var driver = CSharpGeneratorDriver.Create(new[] { generator });
 
-        // To run generators, we can use an empty compilation.
-        var compilation = CSharpCompilation.Create(className,
-            new[] { CSharpSyntaxTree.ParseText(source) },
-            new[]
-            {
-                // To support 'System.Attribute' inheritance, add reference to 'System.Private.CoreLib'.
-                MetadataReference.CreateFromFile(typeof(object).Assembly.Location)
-            });
+    //    // To run generators, we can use an empty compilation.
+    //    var compilation = CSharpCompilation.Create(className,
+    //        new[] { CSharpSyntaxTree.ParseText(source) },
+    //        new[]
+    //        {
+    //            // To support 'System.Attribute' inheritance, add reference to 'System.Private.CoreLib'.
+    //            MetadataReference.CreateFromFile(typeof(object).Assembly.Location)
+    //        });
 
-        // Run generators. Don't forget to use the new compilation rather than the previous one.
-        var runResult = driver.RunGenerators(compilation).GetRunResult();
+    //    // Run generators. Don't forget to use the new compilation rather than the previous one.
+    //    var runResult = driver.RunGenerators(compilation).GetRunResult();
 
-        return runResult;
-    }
+    //    return runResult;
+    //}
 
 
 
