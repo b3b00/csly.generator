4777a2dd5f06eee65e52e85da6fdcd87406c9a5c
b3b00 <olivier.duhart@gmail.com>
2025-12-22 11:09:44 +0100

    removing debug code

--- src/csly.generator/sourceGenerator/staticParserTemplates/lexer/MainLexerTemplate.cs
+++ src/csly.generator/sourceGenerator/staticParserTemplates/lexer/MainLexerTemplate.cs
@@ -29,16 +29,16 @@ public class <#LEXER#>_MainLexer
         int count = 0;
         while (position.Index<source.Length)
         {
-            Console.WriteLine($"\n==============================");
-            Console.WriteLine($"=== #{count++} ====");
-            Console.WriteLine($"=== ");
-            Console.WriteLine($"\n=== mode [{lexerStack.Peek().Name}]");
-            Console.WriteLine($"=== position {position} ");
-            Console.WriteLine($"=== Index={position.Index} / {source.Length}");
-            Console.WriteLine($"=== remaining source: '{source.Slice(position.Index).ToString()}'");
-            Console.WriteLine($"==============================");
-            Console.WriteLine($"=== {string.Join(", ", tokens.Select(x => "[" + x.Value.ToString() + "]"))}");
-            Console.WriteLine($"==============================");
+            //Console.WriteLine($"\n==============================");
+            //Console.WriteLine($"=== #{count++} ====");
+            //Console.WriteLine($"=== ");
+            //Console.WriteLine($"\n=== mode [{lexerStack.Peek().Name}]");
+            //Console.WriteLine($"=== position {position} ");
+            //Console.WriteLine($"=== Index={position.Index} / {source.Length}");
+            //Console.WriteLine($"=== remaining source: '{source.Slice(position.Index).ToString()}'");
+            //Console.WriteLine($"==============================");
+            //Console.WriteLine($"=== {string.Join(", ", tokens.Select(x => "[" + x.Value.ToString() + "]"))}");
+            //Console.WriteLine($"==============================");
             var currentLexer = lexerStack.Peek();
     var scanResult = currentLexer.Scan(source, position);
     var result = scanResult.Result;
@@ -49,8 +49,8 @@ public class <#LEXER#>_MainLexer
             }
 
 // add tokens
-Console.WriteLine($"Lexer mode {currentLexer.Name} produced {result.Tokens.Count} tokens ");
-    Console.WriteLine($"Tokens: \n- {string.Join("\n- ", result.Tokens.Select(x => x.ToString()))}");
+//Console.WriteLine($"Lexer mode {currentLexer.Name} produced {result.Tokens.Count} tokens ");
+//    Console.WriteLine($"Tokens: \n- {string.Join("\n- ", result.Tokens.Select(x => x.ToString()))}");
 tokens.AddRange(result.Tokens);
 
             // update position
@@ -65,12 +65,12 @@ tokens.AddRange(result.Tokens);
                     return new LexicalError("Cannot pop the last lexer");
                 }
                 lexerStack.Pop();
-                Console.WriteLine($"<<< popping to mode [{lexerStack.Peek().Name}] ");
+                //Console.WriteLine($"<<< popping to mode [{lexerStack.Peek().Name}] ");
 
             }
             else if (!string.IsNullOrEmpty(scanResult.PushTarget))
 {
-    Console.WriteLine($">>> Pushing lexer mode to [{scanResult.PushTarget}] ");
+    //Console.WriteLine($">>> Pushing lexer mode to [{scanResult.PushTarget}] ");
     if (!SubLexers.ContainsKey(scanResult.PushTarget))
     {
         return new LexicalError($"Unknown lexer mode to push: {scanResult.PushTarget}");
