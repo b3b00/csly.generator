eda3dfda600fc40188cc83df78fee1a3a10ccdc5
b3b00 <olivier.duhart@gmail.com>
2026-03-08 20:09:02 +0100

    backtrack grammar unit tests

---
+++ tests/generatorTests/BacktrackTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using backtrack;
+using NFluent;
+
+
+namespace ParserTests.samples;
+
+[CollectionDefinition("backtrack", DisableParallelization = true)]
+public class BacktrackTests
+{
+
+
+
+    public BackTrackParserMain BuildParser()
+    {
+        var parser = new BackTrackParserMain(new BackTrackParser(), useMemoization: true);
+        return parser;
+    }
+
+    [Fact]
+    public void Parse_Backtrack()
+    {
+        var parser = BuildParser(); 
+        var result = parser.Parse("funA(funB(C == 2));");
+        Check.That(result).IsNotNull();
+        Check.That(result.IsOk).IsTrue();
+        Check.That(result.Result).IsEqualTo("funA(funB(C==2));");
+    }
+
+
+    
+}
