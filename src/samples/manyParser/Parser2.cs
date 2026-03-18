db6e7de900e0c96e0f87c0b0f32ace437812ea08
b3b00 <olivier.duhart@gmail.com>
2026-03-18 18:09:39 +0100

    many parser : initial

---
+++ src/samples/manyParser/Parser2.cs
@@ -0,0 +1,37 @@
+﻿
+using csly.manyParser.models;
+
+namespace manyParser;
+
+[ParserGenerator]
+public partial class Parser2Generator : AbstractParserGenerator<Token2, Parser2 , string>
+{
+}
+
+[ParserRoot("root2")]
+public class Parser2
+{
+
+    [Production("root2 : ID ints2")]
+    public string Root1(Token<Token1> id, string ids)
+    {
+        return $"{id.Value} : {ids}";
+    }
+
+    [Production("ids: INT*")]
+    public string ints(List<Token<Token2>> ids)
+    {
+        return string.Join(" ", ids.Select(i => i.Value));
+    }
+    
+}
+
+public enum Token1
+{
+    
+    [AlphaId]
+    ID1,
+    
+    [Int]
+    INT1
+}
